d0a0e4cd267fa5c8f96b52f298e75a240be38a1f
euijeong20 <euijeong20@naver.com>
2025-02-19 15:43:51 +0900

    asda

---
+++ Assets/Scripts/HEJ/UIM.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIM : MonoBehaviour
+{
+    RawImage star2;
+    RectTransform star;
+
+    public Transform player;
+    Vector2 player2;
+
+    private void Awake()
+    {
+        star2 = GetComponent<RawImage>();
+        star = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        player2 = star.anchoredPosition;
+        Debug.Log(player2);
+    }
+}
