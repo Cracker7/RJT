1f1a806b554bd32aa9aad7f9ab915c98b97e3723
DESKTOP-64NT6NA\admin <kjy2000212@gmail.com>
2025-02-19 14:42:32 +0900

    사막안개

--- Assets/Scripts/KJY/RGTWaypoint.cs
+++ Assets/Scripts/KJY/RGTWaypoint.cs
@@ -3,35 +3,16 @@ using UnityEngine.AI;
 
 public class RGTWaypoint : MonoBehaviour
 {
-    public Transform[] waypoints; // 웨이포인트 배열
-    private int currentWaypointIndex = 0; // 현재 목표 웨이포인트
+    [SerializeField] private Transform originTrs; // 사용할 위치 배열
 
-    private NavMeshAgent agent;
 
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        transform.rotation = Quaternion.Euler(0f,0f,0f);
-        agent = GetComponent<NavMeshAgent>();
-        MoveToNextWaypoint();
-    }
-
-    void Update()
-    {
-        // 목적지에 거의 도착하면 다음 웨이포인트로 이동
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        if(other.gameObject.layer == LayerMask.NameToLayer("carbody"))
         {
-            MoveToNextWaypoint();
+            other.gameObject.transform.position = originTrs.position;
         }
     }
 
-    void MoveToNextWaypoint()
-    {
-        if (waypoints.Length == 0)
-            return;
 
-        agent.SetDestination(waypoints[currentWaypointIndex].position);
-
-        // 다음 웨이포인트 인덱스 설정 (순환 방식)
-        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
-    }
 }
