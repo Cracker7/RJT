e121bc832a99fa172109c381daa09bbec4bdc976
DESKTOP-QNNOQTD\user <dndb0413@naver.com>
2025-02-25 03:40:45 +0900

    오프닝씬 완료/ 로비 씬 오프닝씬 메인씬 연결 완료/ 오디오매니저 수정/ 설산 메인 맵 가져온 후 미수정/ P키 누르면 바로 엔딩 씬 보는 스크립트 수정

--- Assets/Scripts/CDH/ClearEndToLobby.cs
+++ Assets/Scripts/CDH/ClearEndToLobby.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class ClearEndToLobby : MonoBehaviour
 {
-    public GameObject clearEnd;  // 게임 클리어 이미지(3D 오브젝트)
+    public GameObject clearEndImage;  // Main에서 게임 클리어 후 Lobby로 돌아오면 띄 울 이미지
 
     void Start()
     {
         // 게임 클리어 여부 확인
         if (PlayerPrefs.GetInt("GameCleared", 0) == 1)
         {
-            // 게임 클리어 이미지 활성화
-            clearEnd.SetActive(true);
+            // 게임 클리어가 됐다면 이미지 활성화
+            clearEndImage.SetActive(true);
         }
         else
         {
-            // 게임 클리어 이미지 비활성화
-            clearEnd.SetActive(false);
+            // 게임 클리어가 안됐다면 이미지 비활성화
+            clearEndImage.SetActive(false);
         }
     }
 }
--- Assets/Scripts/CDH/EndCheck.cs
+++ Assets/Scripts/CDH/EndCheck.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class EndCheck : MonoBehaviour
 {
     public GameObject EndCutScene;
+    //public GameObject UIs; // 게임 실행 중 뜨는 UI들 모음
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "carbody")
         {
             EndCutScene.SetActive(true);
+            // UIs.SetActive(false); // 엔딩 컷씬 동안 UI 끄기
 
             PlayerPrefs.SetInt("GameCleared", 1); //엔딩 본 후 UI변경을 위해서 저장
             PlayerPrefs.Save(); // 저장
