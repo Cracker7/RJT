c577acbf234002b9163a9d843bb04a0f35c69f41
DESKTOP-RG5KEL1\admin <hjgr.lordcreep@gmail.com>
2025-02-24 19:15:57 +0900

    카메라

--- Assets/Scripts/KMS/PlayerKMS.cs
+++ Assets/Scripts/KMS/PlayerKMS.cs
@@ -9,6 +9,8 @@ public class PlayerKMS : MonoBehaviour
 {
     public static int DeadCnt = 0;
     public CinemachineCamera JumpCam;
+    public CinemachineCamera followCam;
+    public Transform followPlayer;
     // 상태 머신
     public enum PlayerState { Idle, Transitioning, Riding, Dead }
     public PlayerState currentState = PlayerState.Idle;
@@ -404,7 +406,9 @@ public class PlayerKMS : MonoBehaviour
         // 이동 중에는 입력과 이동을 비활성화
         currentInput = null;
         currentMovement = null;
-
+        followCam.Follow = null;
+        StartCoroutine(SwitchCameraWithDelay());
+        StartCoroutine(CameraZoomEffect());
         StartCoroutine(SwitchCameraWithDelay());
         //JumpCam.Priority = 15;
         Time.timeScale = 0.5f;
@@ -924,7 +928,8 @@ public class PlayerKMS : MonoBehaviour
     private IEnumerator SwitchCameraWithDelay()
     {
         yield return new WaitForSeconds(0.5f);
-
+        followCam.Lens.FieldOfView = 60;
+        //followCam.Follow = followPlayer;
         JumpCam.Priority = 15;
         Time.timeScale = 1;
     }
@@ -959,6 +964,29 @@ public class PlayerKMS : MonoBehaviour
                 Arrow.gameObject.SetActive(true);
                 break;
         }
+    }
+
+    IEnumerator SmoothZoomIn(float zoomFOV, float duration)
+    {
+        float startFOV = followCam.Lens.FieldOfView;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            followCam.Lens.FieldOfView = Mathf.Lerp(startFOV, zoomFOV, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        followCam.Lens.FieldOfView = zoomFOV;
+    }
+
+    IEnumerator CameraZoomEffect()
+    {
+        yield return StartCoroutine(SmoothZoomIn(20f, 0.2f)); // 0.5초 동안 줌인
+        //yield return new WaitForSeconds(0.2f); // 폭발 장면 유지
+        followCam.Follow = followPlayer;
+        yield return StartCoroutine(SmoothZoomIn(60f, 0.2f)); // 다시 원래대로
     }
 
     private IInputHandler AddRandomInputHandler(GameObject target)
