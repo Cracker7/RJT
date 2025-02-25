972260c3acf5e5f2a67a4213b67262ec26e74416
DESKTOP-HO4TLUV\user <dndb0413@naver.com>
2025-02-25 13:21:49 +0900

    엔딩씬 따로 씬 분리

--- Assets/Scripts/CDH/EndCheck.cs
+++ Assets/Scripts/CDH/EndCheck.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndCheck : MonoBehaviour
 {
-    public GameObject EndCutScene;
+    //public GameObject EndCutScene;
+    //public GameObject MainUI;
+    //public GameObject SnowMap;
+
     //public GameObject UIs; // 게임 실행 중 뜨는 UI들 모음
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "carbody")
         {
-            EndCutScene.SetActive(true);
-            // UIs.SetActive(false); // 엔딩 컷씬 동안 UI 끄기
+            //EndCutScene.SetActive(true);
+            //MainUI.SetActive(false);
+            //SnowMap.SetActive(false);
+
+            //// UIs.SetActive(false); // 엔딩 컷씬 동안 UI 끄기
 
             PlayerPrefs.SetInt("GameCleared", 1); //엔딩 본 후 UI변경을 위해서 저장
             PlayerPrefs.Save(); // 저장
+
+            SceneManager.LoadScene("EndScene");
+
         }
     }
 }
