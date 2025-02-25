5e2dd1a6c1f9c1d4a6f6dc788dfdc933de12c01b
DESKTOP-HO4TLUV\user <dndb0413@naver.com>
2025-02-25 13:22:33 +0900

    충돌

--- Assets/Scripts/CDH/CheatKey/CheatKeyToEnd.cs
+++ Assets/Scripts/CDH/CheatKey/CheatKeyToEnd.cs
@@ -1,18 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CheatKeyToEnd : MonoBehaviour
 {
-    public GameObject EndCutScene;
+    //public GameObject EndCutScene;
+    //public GameObject MainUI;
+    //public GameObject SnowMap;
+    //public GameObject DesertMap;
+    //public GameObject ForestMap;
+
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.P))
         {
-            EndCutScene.SetActive(true);
+            //EndCutScene.SetActive(true);
+            //MainUI.SetActive(false);
+            //SnowMap.SetActive(false);
+            //DesertMap.SetActive(false);
+            //ForestMap.SetActive(false);
 
             PlayerPrefs.SetInt("GameCleared", 1); //엔딩 본 후 UI변경을 위해서 저장
             PlayerPrefs.Save(); // 저장
+
+            SceneManager.LoadScene("EndScene");
+
         }
-            
+
     }
 }
